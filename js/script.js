759d94f70b333fd8f536fb79d2c42e1410d6265e
Magdalena Marchewka <madilenaa@gmail.com>
2020-02-02 22:40:32 +0100

    add const and functions to insertAdjacentHTML

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
   console.log('links:', links);
 }); */
 'use strict';
-
+{
 const titleClickHandler = function(event){
   const clickkedElement = this;
   event.preventDefault();
@@ -37,4 +37,39 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+
+//zad 6.4
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML='';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles){
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="# ' + articleId +'"><span>'+articleTitle +'</span></a></li>';
+    console.log(linkHTML);
+    /* insert link into titleList */
+    titleList.insertAdjacentHTML(beforeend, linkHTML);
+}
+
+generateTitleLinks();
+
 }
